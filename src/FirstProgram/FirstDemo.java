6232f370ece7b2f044f9ed1ab36e2028069042d5
Sarika Wable <sarika.wable09@gmail.com>
2022-12-15 17:56:17 +0530

    Adding Space in Launch Browser

---
+++ src/FirstProgram/FirstDemo.java
@@ -0,0 +1,14 @@
+package FirstProgram;
+
+public class FirstDemo {
+
+	public static void main(String[] args) 
+	{
+		System.out.println("Hello");
+		int a =10;
+		int b=20;
+		int sum=a+b;
+		System.out.println("Additionis :- "+sum);
+	}
+
+}
